c835cd5068583eb9977ed4debb9c8f668a941d2b
dali <dadinbh@gmail.com>
2022-07-17 05:15:24 +0100

    your message

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.views.static import serve
 
 urlpatterns = [
